b4592ef9fafe4467210df46394b183fc7df002dc
MorganGrundy <morgan.j.g@live.com>
2021-03-28 17:22:44 +0100

    Improved descender calculation for FontMetric (#2)
    FontMetric now has image of text and bounds.
    
    Getting all connected components of text image and calculated the
    expected number of components based on text, should then be able to
    eliminate outliers and calculate font metrics.

--- logic/fontmetric.cpp
+++ logic/fontmetric.cpp
@@ -2,68 +2,100 @@
 
 #include <iostream>
 
+#include <opencv2/imgproc.hpp>
+
 #include <QCoreApplication>
 
-FontMetric::FontMetric(const std::string t_text, const int t_height, const int t_baseline)
-    : text{t_text}, height{t_height},
-    ascender{0}, capital{0}, median{0},
-    baseline{t_baseline},
-    descender{0}
+FontMetric::FontMetric(const cv::Mat t_image, const cv::Rect t_bounds, const std::string t_text,
+                       const int t_baseline)
+    : image{t_image}, bounds{t_bounds}, text{t_text}, baseline{t_baseline}, descender{0}
 {
+    //Baseline must be atleast 2
+    if (baseline < 2)
+        throw std::invalid_argument("FontMetric baseline must be atleast 2");
+
+    //Get text properties
     for (auto character: text)
         properties.push_back(CharProperty(character));
 
-    descender = height - baseline;
-
-    //If any character in text reaches ascender then calculate ascender
-    for (auto property: properties)
+    //Find which char property in text
+    bool hasAscender = false;
+    bool hasCapital = false;
+    bool hasMedian = false;
+    //
+    bool hasBaseline = false;
+    bool hasDescender = false;
+    for (const CharProperty property: properties)
     {
         if (property.topPosition == CharProperty::Top::Ascender)
+            hasAscender = true;
+        else if (property.topPosition == CharProperty::Top::Capital)
+            hasCapital = true;
+        else if (property.topPosition == CharProperty::Top::Median)
+            hasMedian = true;
+
+        if (property.bottomPosition == CharProperty::Bottom::Baseline)
+            hasBaseline = true;
+        else if (property.bottomPosition == CharProperty::Bottom::Descender)
+            hasDescender = true;
+    }
+
+    //Get part of image containing text
+    const cv::Mat textImage = image(bounds);
+
+    //Get connected components of text
+    cv::Mat labelImage(textImage.size(), CV_16U);
+    int nLabels = cv::connectedComponents(textImage, labelImage, 8);
+
+    //Calculate expected number of components
+    int expectedComponents = text.length();
+    //Characters with more than one component
+    for (const char character: text)
+    {
+        //2 components = !":;=?ij
+        if (character == '!' || character == '"' || character == ':' || character == ';' ||
+            character == '=' || character == '?' || character == 'i' || character == 'j')
+            ++expectedComponents;
+        //3 components = %
+        else if (character == '%')
+            expectedComponents += 2;
+    }
+
+    //If we have the expected number of components then getting font metrics is easier :D
+
+    //Get all labels at baseline
+    std::vector<bool> baselineLabels(nLabels, false);
+    uchar *labelPtr;
+    //Check two pixels above and below baseline
+    for (int y = baseline - 2; y <= baseline + 2; ++y)
+    {
+        labelPtr = labelImage.ptr<uchar>(y);
+        for (int x = 0; x < labelImage.cols; ++x)
         {
-            ascender = baseline;
-            break;
+            if (labelPtr[x] != 0 && !baselineLabels.at(labelPtr[x]))
+                baselineLabels.at(labelPtr[x]) = true;
         }
     }
 
-    //If no character reaches ascender then check for capital
-    if (ascender == 0)
+    //Calculate descender
+    if (hasDescender)
     {
-        for (auto property: properties)
+        //Find lowest point with a label that touches baseline
+        for (int y = bounds.height - 1; y > baseline && descender == 0; ++y)
         {
-            if (property.topPosition == CharProperty::Top::Capital)
+            labelPtr = labelImage.ptr<uchar>(y);
+            for (int x = 0; x < labelImage.cols; ++x)
             {
-                capital = baseline;
-                break;
-            }
-        }
-
-        //If no character reaches capital then check for median
-        if (capital == 0)
-        {
-            for (auto property: properties)
-            {
-                if (property.topPosition == CharProperty::Top::Median)
+                if (baselineLabels.at(labelPtr[x]))
                 {
-                    median = baseline;
+                    descender = y - baseline;
                     break;
                 }
             }
-
-            //Cannot obtain matrics from the given text
-            if (median == 0)
-            {
-                std::cerr << __FILE__":" << __LINE__ << " - Could not obtain metrics from given text " << text << "\n";
-                throw std::invalid_argument("Cannot get font metrics from text: " + text);
-            }
-            else
-                ascender = median * 1.6;
         }
-        else
-            ascender = capital * 1.1;
     }
-}
-
-FontMetric::FontMetric(const std::string t_text, const cv::Mat t_img, const int t_baseline)
-{
-
+    else
+    {
+        //Guess descender
+    }
 }
--- logic/fontmetric.h
+++ logic/fontmetric.h
@@ -11,23 +11,26 @@
 class FontMetric
 {
 public:
-    FontMetric(const std::string t_text, const int t_height, const int t_baseline);
-    FontMetric(const std::string t_text, const cv::Mat t_img, const int t_baseline);
+    FontMetric(const cv::Mat t_image, const cv::Rect t_bounds, const std::string t_text,
+               const int t_baseline);
+
+private:
+    //Image that contains text
+    cv::Mat image;
+    //Bounds of text in image
+    cv::Rect bounds;
 
     std::string text;
     std::vector<CharProperty> properties;
 
-    int height; //Pixels from top to bottom of text
-
+    //Font metrics
     int ascender; //Pixels above baseline to ascender
     int capital; //Pixels above baseline to capital
     int median; //Pixels above baseline to median
 
-    int baseline; //Pixels from top to baseline
+    int baseline; //Pixels from top of bounds to baseline
 
     int descender; //Pixels below baseline to descender
-
-private:
 };
 
 #endif // FONTMETRIC_H
